99cfe028b2b48da8d1e97177dce281303b589059
Astrid Galarza <astridgalarzam@gmail.com>
2024-08-02 21:48:23 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,23 +1,27 @@
-import socket
+import socket 
 import threading
 
-host = "127.0.0.1" 
+host = "127.0.0.1"
 port = 1900 
-
 server= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port)) 
-
+server.listen()
 print(f"Server running on {host}:{port}")
 
 clientes = []
 usernames = []
 
-def broadcast (mensaje, _cliente):
+def broadcast(mensaje, _cliente):
     for cliente in clientes:
         if cliente != _cliente:
-            cliente.send(mensaje)
+            try:
+                cliente.send(mensaje)
+            except Exception as e:
+                print(f"Error enviando mensaje a cliente: {e}")
+                cliente.close()
+                clientes.remove(cliente)
 
-def manejo_mensajes (cliente):
+def manejo_mensajes(cliente):
     while True:
         try:
             mensaje = cliente.recv(1024)
@@ -25,26 +29,27 @@ def manejo_mensajes (cliente):
         except:
             index = clientes.index(cliente)
             username = usernames[index] 
-            broadcast(f"ChatBot: {username} esta desconectad".encode('utf-8')) 
+            broadcast(f"{username} se ha desconectado.".encode('utf-8'), cliente)
             clientes.remove(cliente)
             usernames.remove(username)
             cliente.close()
             break   
-
+ 
 def recibir_conexion():
     while True:
         cliente, address = server.accept()
         cliente.send("@username".encode("utf-8"))
-        username = cliente.recv(1024).decode ("utf-8") 
-        clientes.append(cliente) 
+        username = cliente.recv(1024).decode ("utf-8")
+        clientes.append(cliente)
         usernames.append(username)
 
         print (f"{username} esta conectado con {str(address)}")
 
-        mensaje = f"{username} se unio al chat".encode("utf-8") 
+        mensaje = f"{username} se unio al chat".encode("utf-8")
         broadcast(mensaje, cliente)
         cliente.send("Conectado al servidor".encode("utf-8")) 
+
         thread = threading.Thread(target=manejo_mensajes, args=(cliente,))
-        thread.start() 
+        thread.start()
 
 recibir_conexion()
